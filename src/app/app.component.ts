8b5bedb7ba54de6350b5bc15e4269bde040cf765
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-05-08 01:24:05 -0500

    basic CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ObjAddEditComponent} from "./public/obj-add-edit/obj-add-edit.component";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {EmployeeService} from "./services/employee.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { //implements OnInit
   title = 'CRUDexercise';
+
+  constructor(private _dialog: MatDialog,
+              private _empService:EmployeeService){}
+
+  // ngOnInit():void {
+  //   this.getEmployeeList();
+  // }
+
+  openAddEditEmpForm(){
+    const dialogRef=this._dialog.open(ObjAddEditComponent);
+    dialogRef.afterClosed().subscribe({
+      next:(val)=>{
+        if(val){
+          this._empService.getEmployee();
+        }
+      }
+    })
+
+  }
+  // getEmployeeList(){
+  //   this._empService.getEmployee().subscribe({
+  //     next:(res)=>{
+  //       console.log(res)
+  //     },
+  //     error:(err)=>{
+  //        console.log(err)
+  //     }
+  //   });
+  // }
+
+
 }
